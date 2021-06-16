e33f1ff21c7d902d5fe4039731e640ac2aeebe45
Nguyen Trung Hieu <trunghieu99tt@gmail.com>
2021-06-16 10:16:11 +0700

    test deploy

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ import { SERVER_PORT } from './common/config/secrets';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(process.env.SERVER_PORT || SERVER_PORT || 3020, '0.0.0.0');
+  await app.listen(process.env.PORT || SERVER_PORT || 3020, '0.0.0.0');
 }
 bootstrap();
